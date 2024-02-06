a2edfef136c93b254b79df4270b9179f18674282
alexandrefreitasdasilva <alexfreitasbiotec@gmail.com>
2024-02-06 15:10:09 -0300

    update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='ViralFlow',
-      version='1.0.1',
+      version='1.0.2',
       description='''
       Workflows for viral genome Assembly at FioCruz/IAM
       ''',
