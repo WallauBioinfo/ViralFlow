d41d6288de6b3cca6b7cd2c7d187d0774e258214
alexandrefreitasdasilva <92752518+alexandrefreitasdasilva@users.noreply.github.com>
2024-02-06 21:52:03 -0300

    Update version
    Co-authored-by: dezordi <zimmer.filipe@gmail.com>

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='ViralFlow',
-      version='1.0.2',
+      version='1.1.0',
       description='''
       Workflows for viral genome Assembly at FioCruz/IAM
       ''',
