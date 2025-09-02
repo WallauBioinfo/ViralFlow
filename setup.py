4379cf6f90a3bbf92adcf955c8ac0b724305c7e4
AMarinhoSN <ad45@sanger.ac.uk>
2025-09-02 22:39:38 +0100

    Update .gitignore to exclude .nextflow.log files and correct project URL in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(name='ViralFlow',
       description='''
       Workflows for viral genome Assembly at FioCruz/IAM
       ''',
-      url='https://github.com/dezordi/ViralFlow/',
+      url='https://github.com/WallauBioinfo/ViralFlow',
       author='Antonio Marinho & Filipe Z. Dezordi',
       author_email='amarinhosn@pm.me & zimmer.filipe@gmail.com',
       py_modules = [],
