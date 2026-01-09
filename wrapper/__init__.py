3bdae670d07f1c83febe35298065077703a3aa52
dezordi <zimmer.filipe@gmail.com>
2026-01-09 10:30:27 -0300

    fix pangolin image name

--- wrapper/__init__.py
+++ wrapper/__init__.py
@@ -117,12 +117,12 @@ def parse_params(in_flpath):
 
 def update_pangolin(root_path):
     cd_to_dir= f"cd {root_path}/vfnext/containers/" 
-    run_update = "singularity exec --writable ./pangolin_latest.sif pangolin --update"
+    run_update = "singularity exec --writable ./pangolin:4.3.sif pangolin --update"
     os.system(cd_to_dir+';'+run_update)
 
 def update_pangolin_data(root_path):
     cd_to_dir= f"cd {root_path}/vfnext/containers/" 
-    run_update_data = "singularity exec --writable ./pangolin_latest.sif pangolin --update-data"
+    run_update_data = "singularity exec --writable ./pangolin:4.3.sif pangolin --update-data"
     os.system(cd_to_dir+';'+run_update_data)
 
 def run_vfnext(root_path, params_fl):
